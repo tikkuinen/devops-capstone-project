b081a9062ea19b863d6ddbb9c0415b743a76b766
tikkuinen <titunen@muak.fi>
2025-01-26 14:43:27 -0500

    final linting errors

--- service/routes.py
+++ service/routes.py
@@ -66,12 +66,8 @@ def create_accounts():
 
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
-    """
-    List all Accounts
-    This endpoint will list all Accounts
-    """
-    app.logger.info("Request to list Accounts")
-
+    """Lists all accounts"""
+    app.logger.info("Request to list accounts")
     accounts = Account.all()
     account_list = [account.serialize() for account in accounts]
 
@@ -86,16 +82,11 @@ def list_accounts():
 
 @app.route("/accounts/<int:account_id>", methods=["GET"])
 def get_accounts(account_id):
-    """
-    Reads an Account
-    This endpoint will read an Account based the account_id that is requested
-    """
-    app.logger.info("Request to read an Account with id: %s", account_id)
-
+    """Reads an account based on the given id"""
+    app.logger.info("Request to read an account with id: %s", account_id)
     account = Account.find(account_id)
     if not account:
-        abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
-
+        abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found")
     return account.serialize(), status.HTTP_200_OK
 
 
@@ -106,19 +97,16 @@ def get_accounts(account_id):
 
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
 def update_accounts(account_id):
-    """
-    Update an Account
-    This endpoint will update an Account based on the posted data
-    """
+    """ Update an Account"""
     app.logger.info("Request to update an Account with id: %s", account_id)
-
+    
     account = Account.find(account_id)
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
-
+    
     account.deserialize(request.get_json())
     account.update()
-
+    
     return account.serialize(), status.HTTP_200_OK
 
 
@@ -129,16 +117,12 @@ def update_accounts(account_id):
 
 @app.route("/accounts/<int:account_id>", methods=["DELETE"])
 def delete_accounts(account_id):
-    """
-    Delete an Account
-    This endpoint will delete an Account based on the account_id that is requested
-    """
-    app.logger.info("Request to delete an Account with id: %s", account_id)
-
+    """Delete an account based on the requested id"""
+    app.logger.info("Request to delete an account with id: %s", account_id)
     account = Account.find(account_id)
     if account:
         account.delete()
-
+    
     return "", status.HTTP_204_NO_CONTENT
 
 
